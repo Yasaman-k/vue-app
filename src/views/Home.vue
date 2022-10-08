63a3312bb42c7c3064c1f0901e20dda5c4aaff1e
Yasaman K <yasaman@m.lo.agency>
2022-10-08 21:32:30 +0330

    feat: add data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,73 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+        <div
+          v-for="(product, i) in inventory.slice(0, 3)"
+          :key="product.id"
+          class="card"
+        >
+          <div class="card-title">
+            {{ product.name }}
+          </div>
+          <div class="card-body">
+            <i class="icofont-10x icofont-{{ product.icon }}"></i>
+            <form>
+              <div class="row">
+                <div class="cell">
+                  <label>Type:</label>
+                </div>
+                <div class="cell">
+                  <em>{{ product.type }}</em>
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Price:</label>
+                </div>
+                <div class="cell">${{ product.price.USD }}</div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Quantity:</label>
+                </div>
+                <div class="cell">
+                  <input type="number" v-model.number="product.quantity" />
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-footer">
+            <button @click="addToCart(product.name, i)" class="btn btn-light">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import food from '@/food.json'
 
 export default {
   name: 'Home',
-  components: {
-  }
+  data () {
+    return {
+      inventory: food
+    }
+  },
+  components: {}
 }
 </script>
