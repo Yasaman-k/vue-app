387f57a1c292ba114a1a2a4ca4e6ab99c10a0407
Yasaman K <yasaman@m.lo.agency>
2022-10-09 17:34:15 +0330

    feat: create product card component

--- src/App.vue
+++ src/App.vue
@@ -46,10 +46,9 @@ export default {
     }
   },
   methods: {
-    addToCart(name, index) {
+    addToCart(name, quantity) {
       if (!this.cart[name]) this.cart[name] = 0
-      this.cart[name] += this.inventory[index].quantity
-      this.inventory[index].quantity = 0
+      this.cart[name] += quantity
     },
     toggleSidebar() {
       this.showSidebar = !this.showSidebar
